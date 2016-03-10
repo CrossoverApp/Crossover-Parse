c74dcddebbfc3c0e3cc9273bbc0e54032c42c0a9
TimMa64 <ttma@ucsc.edu>
2016-03-09 17:10:51 -0800

    Taking out unecessary code

--- public/js/tabs.js
+++ public/js/tabs.js
@@ -178,69 +178,9 @@ $("#open_selected_rows").click(function() {
   })
   
 
-//   //New Tab Group Button
-//   $("#newTabGroupButton").click(function () {
-// 		var tabTitle = ""
-//     swal({
-//       title: "New Tab Group",
-//       text: "Please enter a name for your new tab group:",
-// 			html: '<p style="padding-bottom: 20px">Enter a name for your new Tab Group: </p> <p><input id="input-field" style="margin: 10px; padding: 5px; font-size: 20px;"></p>',
-//       showCancelButton: true,
-//       closeOnConfirm: false,
-//       confirmButtonColor: '#3085d6',
-//       cancelButtonColor: '#d33',
-//       inputPlaceholder: "Write something",
-//       showLoaderonConfirm: true
-//     }, function() {
-//       if ($('#input-field').val() === false) return false;
-//       if ($('#input-field').val() === "") {
-//         swal(   'Woops!',   'You need to enter something!',   'error' )
-//         return false
-//       }
-// 			tabTitle = $('#input-field').val()
-//       $.post('/newTabGroup', {
-//         title: $('#input-field').val()
-//       }, function(response){
-//         if(response.success) {
-//           swal({
-//             title: 'Nice!',
-//             text: "Your new group, \"" + tabTitle+"\", was created!",
-//             type: 'success'
-//           }, function() {
-//             location.reload()
-//           });
-//         } else {
-//           swal("Oh no!", "We were unable to create your new Tab group. Please try again.", "error")
-//         }
-//       })
-      
-      
-//     })
-//   })
 
-// //Delete Tab Group
-// $("#deleteTabGroupButton").click(function () {
-//        swal({
-//             title: 'Delete Tab Group',
-//             html:'<form>'
-//             + '<select id = "tabGROUP">' 
-//             + '<option id="addTabOption" value=""></option>'
-//             + '</select>'
-//             + '</form>',
-//             showCancelButton: true,
-//             closeOnConfirm: false,
-//             confirmButtonColor: '#3085d6',
-//             cancelButtonColor: '#d33',
-//             showLoaderonConfirm: true
-//           },
-//            function() {
-//             var addTabOption
-//             for(i=0; i < tabGroup.length + 1; i++){
-//               $('#tabGROUP').append("<option value="+ i +"'>"+ tabGroup +"</option>");
-//            }
-//           });
-// });
 
+//Moving the tabs around using a drag and drop menu
 $('#tab_logic').sortable({
   containerSelector: 'table',
   itemPath: '> tbody',
